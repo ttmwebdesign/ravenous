cf1ad9327f1e2fd0faff6916a536e3b09dfc3623
Niki <info@tothemoonwebdesign.com>
2018-01-04 22:51:13 -0700

    Just about finished; images in search results not showing, not sure why

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import BusinessList from './components/BusinessList/BusinessList.js'
-import SearchBar from './components/SearchBar/SearchBar.js'
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import Yelp from './util/Yelp.js';
 
+/*
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
  name: 'MarginOtto Pizzeria',
@@ -16,10 +18,18 @@ const business = {
 };
 
 const businesses = [business, business, business, business, business, business];
+*/
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {businesses: []};
+    this.searchYelp = this.searchYelp.bind(this);
+  }
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Ravenous with ${term}, ${location}, ${sortBy}`)
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
   }
 
   render() {
@@ -27,7 +37,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList businesses={businesses}/> 
+        <BusinessList businesses={this.state.businesses}/> 
       </div>
     );
   }
---
+++ src/util/Yelp.js
@@ -0,0 +1,29 @@
+const apiKey = 'E-wQxgcbAQ4Oa-P3NwcBFgqNSdlxMUayqPgXiqn8NwU9L8m1UN9dlsDphtuZczZhRISVEBdWIM1wTyRxlzB8c33XgQKqA3UIaATTjWxSfoHcxYnrAOxhp8euHwJPWnYx';
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    {
+        headers: { Authorization: `Bearer ${apiKey}`}
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imgSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                };
+            } );
+        }
+    });
+    }
+};
+export default Yelp;
